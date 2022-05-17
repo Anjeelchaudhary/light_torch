0cfadf6d409ef1d9d50dff59a148c8692a075c3d
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-17 19:48:42 +0545

    :sparkles: Main UI added
    main ui and some account is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:day_night_switcher/day_night_switcher.dart';
+import 'package:torch_light/torch_light.dart';
+//
+import '../widgets.dart';
 
 void main() {
   runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter FlashLight App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: "Oxygen",
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Main(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Main> createState() => _MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MainState extends State<Main> {
+  bool isFlashOn = true;
+  bool isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: isDarkMode
+            ? Color.fromARGB(255, 245, 245, 245)
+            : Color.fromARGB(255, 24, 24, 24),
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Flashy",
+            style: TextStyle(
+                color: isDarkMode ? Colors.black : Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w500),
+          ),
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                bottom: h / 2.2,
+                child: DayNightSwitcher(
+                    isDarkModeEnabled: isFlashOn,
+                    onStateChanged: (val) {
+                      setState(() {
+                        isFlashOn = val;
+                        isDarkMode = !isDarkMode;
+                      });
+
+                      isFlashOn
+                          ? _turnOffFlash(context)
+                          : _turnOnFlash(context);
+                    }),
+              ),
+              Positioned(
+                bottom: h / 7.5,
+                child: Text(
+                  "Made By AmirHosein Bayat",
+                  style: TextStyle(
+                      color: isDarkMode ? Colors.black : Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                child: SizedBox(
+                  width: w / 1.2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      /// My Gmail
+                      bottomSocial(
+                          img: "assets/images/gmail.png",
+                          url: "https://chaudharyanjeel@gmail.com",
+                          context: context,
+                          isDarkMode: isDarkMode),
+
+                      /// My Instagram
+                      bottomSocial(
+                          img: "assets/images/inst.png",
+                          url: "https://www.instagram.com/anjeel.191736/?hl=en",
+                          context: context,
+                          isDarkMode: isDarkMode),
+
+                      /// My GitHub
+                      bottomSocial(
+                          img: "assets/images/github.png",
+                          url: "https://github.com/Anjeelchaudhary",
+                          context: context,
+                          isDarkMode: isDarkMode),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Future<void> _turnOnFlash(BuildContext context) async {
+  try {
+    await TorchLight.enableTorch();
+  } on Exception catch (_) {
+    _showErrorMes('Could not enable Flashlight', context);
+  }
+}
+
+Future<void> _turnOffFlash(BuildContext context) async {
+  try {
+    await TorchLight.disableTorch();
+  } on Exception catch (_) {
+    _showErrorMes('Could not enable Flashlight', context);
+  }
+}
+
+void _showErrorMes(String mes, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mes)));
 }
